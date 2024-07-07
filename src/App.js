8ac9d310d4fe594a8190d4b0e0fa7ec07a062a67
Yousef Hadder <yousefhadder94@gmail.com>
2024-07-07 14:06:43 -0500

    Adding events handling in Form Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const initialItems = [
@@ -19,17 +20,44 @@ function Logo() {
 	return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+	const [description, setDescription] = useState('');
+	const [quantity, setQuantity] = useState(1);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		if (!description) return;
+
+		const newItem = {
+			id: Date.now(),
+			description,
+			quantity,
+			packed: false,
+		};
+
+		console.log('Adding new item: ', newItem);
+
+		setDescription('');
+		setQuantity(1);
+	};
 	return (
-		<form className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>what do you need for your trip?</h3>
-			<select value={1}>
+			<select
+				value={quantity}
+				onChange={(event) => setQuantity(Number(event.target.value))}>
 				{Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
 					<option value={num} key={num}>
 						{num}
 					</option>
 				))}
 			</select>
-			<input type="text" placeholder="Item..." value={''} />
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(event) => setDescription(event.target.value)}
+			/>
 			<button>Add</button>
 		</form>
 	);
